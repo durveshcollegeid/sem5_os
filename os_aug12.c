5c264417458ee18be0b76fec0780f2abe5fab864
durveshcollegeid <durveshcollegeid@gmail.com>
2026-08-12 15:27:28 +0530

    Update os_aug12.c

--- os_aug12.c
+++ os_aug12.c
@@ -1,10 +1,18 @@
-//Q1 : print message twice
-
+//Q2 identify parent and child 
 #include <stdio.h>
 #include <unistd.h>
 
 int main() {
-    fork();
-    printf("Hello");
+    
+    int pid = fork();
+
+    if( pid==0 ) {
+        printf("Hello from parent !");
+    }
+    else {
+        printf("Hello from child !");
+
+    }
+
     return 0;
 }
