611c18488f219bf02735cb2ab64d547cbb683f61
durveshcollegeid <durveshcollegeid@gmail.com>
2026-09-10 09:40:29 +0530

    rectify the bugs related to at,bt assignment

--- fcfs_code.c
+++ fcfs_code.c
@@ -10,7 +10,7 @@ int wt[ 20 ] = {0};
 */
 
 void main(){
-	int pid[ 20 ] = {0};
+int pid[ 20 ] = {0};
 int at[20] = {0};
 int bt[20] = {0};
 int ct[20] = {0};
@@ -24,50 +24,60 @@ int wt[ 20 ] = {0};
 
 	//fill arrays with input data
 	for( int index=0; index<n; index++ ){
+		pid[ index ] = index; 
+
 		printf( "arrival time for p-%d = ", index+1 );
 		scanf( "%d", at + index); printf("\n");
 
 		printf( "burst time for p-%d  = ", index+1 );
-		scanf( "%d", at + index); printf("\n");
+		scanf( "%d", bt + index); printf("\n");
 	}
 
 	//sort processes in ascending order of arrival time
 	for( int i = 0; i < n-1; i++ ){
 		for( int j = i+1; j < n; j++ ){
-		if( at[i] > at[j] ){
+		if( at[i] > at[j] || ( (at[i] == at[j]) && (pid[i] > pid[j]) ) ){
 			int temp = 0;
 
 			temp = at[i];
-			at[ j ] = at[i];
-			at[i] = temp;
+			at[ i ] = at[j];
+			at[j] = temp;
 
 			temp = bt[i];
-			bt[ j ] = bt[i];
-			bt[i] = temp;
+			bt[ i ] = bt[j];
+			bt[j] = temp;
 
 			temp = pid[i];
-			pid[ j ] = pid[i];
-			pid[i] = temp;
+			pid[ i ] = pid[j];
+			pid[j] = temp;
 
 			}
 		}
 	}
 
-		int time = 0;
+	float avg_tat = 0; float avg_wt = 0;
+	int time = 0;
 	printf( "pid \t at \t bt \t ct \t tat \t wt\n" );
 	for( int index = 0; index<n; index++ ){
 		//id = pid[ index ];
 
-		ct[ index ] = time + bt[index];
 		time = time + bt[index] ;
+		ct[ index ] = time ;
+		
 
 		tat[ index ] = ct[index] - at[index];
 		wt[index] = tat[index] - bt[index] ;
 
-		printf( "%d \t %d \t %d \t %d \t %d \t %d\n", pid[index]+1, at[index], bt[index], ct[index], tat[index], wt[index] );
+		printf( "P%d \t %d \t %d \t %d \t %d \t %d\n", pid[index]+1, at[index], bt[index], ct[index], tat[index], wt[index] );
 		
+		avg_tat += tat[index];
+		avg_wt += wt[index];
 	}
+		avg_tat = avg_tat/n;
+		avg_wt = avg_wt/n;
 
+	printf( "avg_tat = %.2f\n", avg_tat );
+	printf( "avg_wt = %.2f\n", avg_wt );
 }
 
 
